b251e222f318f49b57c917ed97be1fc35cc309ec
Silvia Escanciano <silvia.escanciano@gmail.com>
2024-02-08 18:58:53 +0100

    first push

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import '../scss/App.scss';
+import Header from "./Header"
+// import { useEffect, useState } from 'react';
+// import callToApi from "../services/api";
+// import ls from '../services/localStorage';
+// import { Link, Route, Routes } from "react-router-dom";
+// import { Link, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+};
+
+export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import header from "../images/header.png"
+
+function Header() {
+  return (
+    <img src={header} alt="harry potter" className="header"/>
+  )
+}
+
+export default Header
